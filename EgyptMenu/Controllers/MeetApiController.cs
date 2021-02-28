0ad3b171bd3dcf7ae1115f8c1a51152675e3c85a
Mahmoud Tiger <mahmoud.tiger.201293@outlook.com>
2021-02-28 16:56:45 +0200

    meetapi

---
+++ EgyptMenu/Controllers/MeetApiController.cs
@@ -0,0 +1,47 @@
+﻿using EgyptMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace EgyptMenu.Controllers
+{
+
+    public class MeetApiController : ApiController
+    {
+        // GET: api/MeetApi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/MeetApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/MeetApi
+        [HttpPost]
+        public void Post([FromUri]string Name)
+        {
+            //var File = model.Image;
+            
+            var File = HttpContext.Current.Request.Files.Count > 0 ? HttpContext.Current.Request.Files[0] : null;           
+            var ext = new FileInfo(File.FileName).Extension;
+            var fullpath = HttpContext.Current.Server.MapPath("~/Content/images/" + Name+ext);            
+            File.SaveAs(fullpath);
+        }
+
+
+
+        // DELETE: api/MeetApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EgyptMenu/Models/MeetApiModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EgyptMenu.Models
+{
+    public class MeetApiModel
+    {
+        public string Username { get; set; }
+        public IFormFile Image { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
